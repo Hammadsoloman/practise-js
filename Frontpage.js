9413be179924172b6ea2da15b10f255f4b7335a8
h@soloman <coding202020@gmail>
2020-02-05 23:46:24 +0200

    New information

--- Frontpage.js
+++ Frontpage.js
@@ -1,15 +1,27 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
-if (hourNow > 18){
-    greeting = 'good evening!';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-}else if(hourNow > 0){
-    greeting ='Good morning!';
-}else {
-    greeting = 'Welcome!';
-}
-
-document.write('<h3>'+greeting+'</h3>');
+var Name = window.prompt("Enter your name pls");
+console.log("The name is=",Name);
+var age = window.prompt("your age pls");
+console.log("The name is=",age);
+var pet = window.prompt("Do you have a pit");
+console.log("The name is=",pet);
+var age1; 
+var note;
+ 
+if (age > 69 ) {
+    note= 'You live alot! go and die!!'
+    age1 = age;
+    console.log("true0")
+  } else if (age >17 ) {
+    note= 'Dont waste your time in this site'
+    age1 = age;
+    console.log("true")
+  } else if (age <= 17) {
+    note= 'you still young Find a Jop dont waste your time in a schoole'
+    console.log("true2")
+    age1 = age;
+  } else {    age1 = 'WT*';
+  console.log("false")
+  }
+ document.write('<h1>  Note:  '+note+'   </h1>');
+ document.write('<h2> Name:    '+Name+'                       Age:   ' +age+'                     Pet Name: '+pet+'</h2>');
+//  the reason is  to print  client data after entring the wibsite.
